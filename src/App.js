14cddde3e6af58f502d3bd311695f8341202bd8f
EV1L46rus <vislik46rus@gmail.com>
2023-02-20 20:57:15 +0300

    axios

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useMemo, useState} from "react";
+import React, {useEffect, useState} from "react";
 import PostForm from "./components/PostForm";
 import PostList from "./components/PostList";
 import "./styles/app.css";
@@ -6,17 +6,32 @@ import PostFilter from "./components/PostFilter";
 import MyModal from "./components/UI/MyModal/MyModal";
 import MyButton from "./components/UI/button/MyButton";
 import { usePosts } from "./hooks/usePosts";
+import PostService from "./API/PostService";
+import Loader from "./components/UI/Loader/Loader";
+import { useFetching } from "./hooks/useFetching";
+import { getPageCount, getPagesArray } from "./utils/pages";
 
 function App() {
-  const [posts, setPosts] = useState([
-    {id: 1, title: 'Javascript', body: 'Description'},
-    {id: 2, title: 'Javascript', body: 'Description'},
-    {id: 3, title: 'Javascript', body: 'Description'},
-  ])
 
+const [posts, setPosts] = useState([])
 const [filter, setFilter] = useState({sort: '', query: ''})
 const [modal, setModal] = useState(false)
+const [totalPages, setTotalPages] = useState(0)
+const [limit, setLimit] = useState(10)
+const [page, setPage] = useState(1)
 const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
+let pagesArray = getPagesArray(totalPages)
+
+const [fetchPosts, isPostsLoading, postError] = useFetching(async() => {
+  const response = await PostService.getAll(limit, page)
+    setPosts(response.data)
+    const totalCount = response.headers['x-total-count'];
+    setTotalPages(getPageCount(totalCount, limit))
+})
+
+useEffect( () => {
+  fetchPosts()
+}, [])
 
 const createPost = (newPost) =>{
   setPosts([...posts, newPost])
@@ -43,8 +58,20 @@ const removePost = (post) =>{
         filter={filter}
         setFilter={setFilter}
       />
-      <PostList remove={removePost} posts={sortedAndSearchedPosts} title={'Посты про JS'}/>
-    </div>
+      {postError &&
+        <h1>Произошла ошибка</h1>
+      }
+      {isPostsLoading
+      ? <div style={{display: 'flex', justifyContent: 'center', marginTop: '25px'}}><Loader/></div>
+      : <PostList remove={removePost} posts={sortedAndSearchedPosts} title={'Посты про JS'}/>
+      }
+      <div style={{marginTop: '30px'}}>
+      {pagesArray.map(p =>
+        <span>{p}</span>  
+      )}
+      </div>
+      
+      </div>
   );
 }
 
